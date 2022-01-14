14105375355e817d268d7bacb868c792d10d7a6b
Takashi Imamichi <imamichi@jp.ibm.com>
2022-01-14 21:48:58 +0900

    add sampler test with pqc

--- test/python/quantum_info/primitives/sampler/test_base_sampler.py
+++ test/python/quantum_info/primitives/sampler/test_base_sampler.py
@@ -18,7 +18,7 @@ from test import combine
 from ddt import ddt
 
 from qiskit import BasicAer, QuantumCircuit
-# from qiskit.circuit.library import RealAmplitudes
+from qiskit.circuit.library import RealAmplitudes
 from qiskit.quantum_info.primitives.results import SamplerResult
 from qiskit.quantum_info.primitives.sampler import BaseSampler
 from qiskit.test import QiskitTestCase
@@ -42,15 +42,21 @@ class TestBaseSampler(QiskitTestCase):
         bell.cx(0, 1)
         bell.measure(0, 0)
         bell.measure(1, 1)
-        # ansatz = RealAmplitudes(num_qubits=2, reps=2)
         self._circuit = [hadamard, bell]
-        self._target = [{"0": 0.5, "1": 0.5}, {"00": 0.5, "11": 0.5, "01": 0, "10": 0}]
+        self._target = [
+            {"0": 0.5, "1": 0.5},
+            {"00": 0.5, "11": 0.5, "01": 0, "10": 0},
+        ]
         self._run_config = {"seed_simulator": 15}
+        self._pqc = QuantumCircuit(2, 2)
+        self._pqc.compose(RealAmplitudes(num_qubits=2, reps=2), inplace=True)
+        self._pqc.measure(0, 0)
+        self._pqc.measure(1, 1)
 
     def _generate_circuits_target(self, indices):
         if isinstance(indices, int):
             circuits = self._circuit[indices]
-            target = [self._target[indices]]
+            target = self._target[indices]
         elif isinstance(indices, list):
             circuits = [self._circuit[j] for j in indices]
             target = [self._target[j] for j in indices]
@@ -73,6 +79,8 @@ class TestBaseSampler(QiskitTestCase):
         return counts
 
     def _compare_probs(self, prob, target):
+        if not isinstance(target, list):
+            target = [target]
         self.assertEqual(len(prob), len(target))
         for p, t in zip(prob, target):
             for key, t_val in t.items():
@@ -112,6 +120,34 @@ class TestBaseSampler(QiskitTestCase):
         with self.subTest("direct call"):
             sampler = BaseSampler(circuits=circuits, backend=backend)
             result = sampler(shots=shots, **self._run_config)
+            self.assertEqual(result.shots, shots)
+            prob = self._counts_to_prob(result.counts)
+            self._compare_probs(prob, target)
+
+    @unittest.skipUnless(has_aer(), "qiskit-aer doesn't appear to be installed.")
+    @combine(
+        params_target=[
+            ([0, 0, 0, 0, 0, 0], {"00": 1}),
+            ([1, 1, 1, 1, 1, 1], {"00": 0.0148, "01": 0.3449, "10": 0.0531, "11": 0.5872}),
+        ],
+        shots=[1000, 2000],
+    )
+    def test_evaluate_pqc_aer(self, params_target, shots):
+        """test for evaluate parametrized circuits"""
+        backend = Aer.get_backend("aer_simulator")
+        params, target = params_target
+        with self.subTest("with-guard"):
+            with BaseSampler(circuits=self._pqc, backend=backend) as sampler:
+                sampler.set_run_options(shots=shots, **self._run_config)
+                result: SamplerResult = sampler(params)
+                self.assertEqual(result.shots, shots)
+                prob = self._counts_to_prob(result.counts)
+                self._compare_probs(prob, target)
+
+        with self.subTest("direct call"):
+            sampler = BaseSampler(circuits=self._pqc, backend=backend)
+            sampler.set_run_options(shots=shots, **self._run_config)
+            result = sampler(params)
             self.assertEqual(result.shots, shots)
             prob = self._counts_to_prob(result.counts)
             self._compare_probs(prob, target)
